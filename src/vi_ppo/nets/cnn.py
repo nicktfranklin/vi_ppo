1db9e5ecb1de906d6a57f7e31a6cab1d62477e7e
Nicholas Franklin <nthompsonfranklin@gmail.com>
2025-02-09 14:27:30 -0500

    code quality

--- src/vi_ppo/nets/cnn.py
+++ src/vi_ppo/nets/cnn.py
@@ -21,7 +21,7 @@ class ConvBlock(nn.Module):
 
 
 @dataclass
-class CnnBlockConfig:
+class CnnConfig:
     input_dims: int
     output_dims: int
     hidden_dims: list[int] = [32, 64, 64]
@@ -30,23 +30,20 @@ class CnnBlockConfig:
     activation: str = "elu"
 
 
-class CnnBlock(nn.Module):
-    def __init__(self, config: CnnBlockConfig):
+class Cnn(nn.Module):
+    def __init__(self, config: CnnConfig):
         super().__init__()
-
-        if kernel_sizes is None:
-            kernel_sizes = [8, 4, 3]
-        if strides is None:
-            strides = [4, 2, 1]
+        self.config = config
 
         modules = []
-        h_in = in_channels
-        for h_dim, kernel_size, stride in zip(channels, kernel_sizes, strides):
+        h_in = config.in_channels
+        for h_dim, kernel_size, stride in zip(
+            config.channels, config.kernel_sizes, config.strides
+        ):
             modules.append(ConvBlock(h_in, h_dim, kernel_size, stride, padding=1))
             h_in = h_dim
 
         self.cnn = nn.Sequential(*modules)
-        self.embedding_dim = embedding_dim
 
     def calculate_output_shape(self, input_shape):
         with torch.no_grad():
@@ -54,7 +51,7 @@ class CnnBlock(nn.Module):
             dummy_input = torch.zeros(1, *input_shape)
             # Pass the dummy input through the CNN layers
             output = self.cnn(dummy_input)
-        return output_shape.shape
+        return output.shape
 
     def forward(self, x):
         # Assume NxCxHxW input or CxHxW input
@@ -88,28 +85,29 @@ class ConvTransposeBlock(nn.Module):
         return x
 
 
-class CnnDecoder(nn.Module):
+@dataclass
+class TransposedCnnConfig:
+    embedding_dim: int = 1024
+    output_channels: int = 1
+    channels: list[int] = [32, 64, 128, 256, 512][::-1]
+
+
+class TransposedCnn(nn.Module):
     def __init__(
         self,
-        embedding_dim: int = 1024,
-        output_channels: int = 1,
-        channels: Optional[List[int]] = None,
-        output_shape=None,  # not used
+        config: TransposedCnnConfig,
     ):
         super().__init__()
 
-        if channels is None:
-            channels = [32, 64, 128, 256, 512][::-1]
-
-        self.fc = nn.Linear(embedding_dim, 4 * channels[0])
-        self.first_channel_size = channels[0]
+        self.fc = nn.Linear(config.embedding_dim, 4 * config.channels[0])
+        self.first_channel_size = config.channels[0]
 
         modules = []
-        for ii in range(len(channels) - 1):
+        for ii in range(len(config.channels) - 1):
             modules.append(
                 ConvTransposeBlock(
-                    channels[ii],
-                    channels[ii + 1],
+                    config.channels[ii],
+                    config.channels[ii + 1],
                     3,
                     stride=2,
                     padding=1,
@@ -120,17 +118,20 @@ class CnnDecoder(nn.Module):
 
         self.final_layer = nn.Sequential(
             nn.ConvTranspose2d(
-                channels[-1],
-                channels[-1],
+                config.channels[-1],
+                config.channels[-1],
                 3,
                 stride=2,
                 padding=1,
                 output_padding=1,
             ),
-            nn.BatchNorm2d(channels[-1]),
+            nn.BatchNorm2d(config.channels[-1]),
             nn.GELU(),
             nn.Conv2d(
-                channels[-1], out_channels=output_channels, kernel_size=3, padding=1
+                config.channels[-1],
+                out_channels=config.output_channels,
+                kernel_size=3,
+                padding=1,
             ),
             nn.Sigmoid(),
         )
